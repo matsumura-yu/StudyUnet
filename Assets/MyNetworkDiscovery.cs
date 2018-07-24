be2c0ca490d59ecb368e323149bf385eea75e1eb
matsumura-yu <matsumura.yu@isid.co.jp>
2018-07-24 16:56:22 +0900

    Add Assets of NetworkDiscovery

---
+++ Assets/MyNetworkDiscovery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkDiscovery : NetworkDiscovery {
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        NetworkManager manager = NetworkManager.singleton;
+
+        if (manager.isNetworkActive)
+            return;
+
+        manager.networkAddress = fromAddress;
+
+        manager.StartClient();
+
+        StartCoroutine(StopBroadcastCoroutine());
+    }
+
+    IEnumerator StopBroadcastCoroutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+        StopBroadcast();
+    }
+
+}
---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class MyNetworkManager : NetworkManager {
+
+    MyNetworkDiscovery m_NetworkDiscovery;
+	// Use this for initialization
+	void Start () {
+        m_NetworkDiscovery = GetComponent<MyNetworkDiscovery>();
+	}
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,250,20), "ホストとして起動"))
+        {
+            StartHost();
+            m_NetworkDiscovery.Initialize();
+            m_NetworkDiscovery.StartAsServer();
+        }
+
+        if(GUI.Button(new Rect(0,30,250,20), "クライアントとして接続"))
+        {
+            m_NetworkDiscovery.Initialize();
+            m_NetworkDiscovery.StartAsClient();
+        }
+    }
+
+    public override void OnStopServer()
+    {
+        m_NetworkDiscovery.StopBroadcast();
+    }
+}
